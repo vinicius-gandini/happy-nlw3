88422be79b31c4a9af170f41c207711f59011ddf
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-10-13 19:14:18 -0300

    feat: init backend project and config database sqlite :ok_hand:

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express, { response } from 'express';
+
+import './database/connection';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    return response.json({ message: "Hello World" })
+})
+
+app.listen(3333);
