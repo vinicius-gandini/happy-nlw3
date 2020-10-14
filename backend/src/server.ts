d48a229bea8fe0a3cb371b530fbd03d66df9c8f0
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-10-13 22:53:11 -0300

    feat: migrations with typeorm and upload images with multer :ok_hand:

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,13 @@
-import express, { response } from 'express';
+import express from 'express';
+import './database/connection';
+
+import routes from './routes';
 
 import './database/connection';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
-    return response.json({ message: "Hello World" })
-})
+app.use(routes);
 
 app.listen(3333);
